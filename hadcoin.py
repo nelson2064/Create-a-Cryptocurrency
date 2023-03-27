94c3373df698fb7f0b12e8edac0d2e7ef16ba681
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-27 12:16:10 +0545

    building a blockchain
    building a blockchain

--- hadcoin.py
+++ hadcoin.py
@@ -16,3 +16,19 @@ from urllib.parse import urlparse
 
 
 
+#general purpose blockchain into crypto currency 
+#> what makes a blockchain a cryptocurrency the answer is transaction the principle of a cryptocurrency is that we're able to exchange these crypto currency thought transactions that are secured added to new blocks mines by the miners in the most secure way that once some transactions are added to a new block the block isintegrated to the block chain and its impossible to modify anything in the blockchain  
+#Consensus in blockchain refers to the process by which multiple nodes on a network agree on the current state of the blockchain ledger. This is necessary to ensure that all nodes have the same version of the ledger and to prevent any fraudulent or malicious activity. In other words, consensus is the mechanism that enables trustless, decentralized systems like blockchain to function.
+############################first##################
+#1.adding the transaction 2.making the consensus
+
+
+# Part 1 - Building a Blockchain
+
+
+
+
+
+
+
+
