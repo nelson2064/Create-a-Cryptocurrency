8572c093d345eb3493375bcbcfa245105d84a850
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-27 12:33:32 +0545

    running the app
    running the app

--- hadcoin.py
+++ hadcoin.py
@@ -344,3 +344,10 @@ def replace_chain():
     #4 
     #next post request whcich is add transaction post request
           #http://127.0.0.1:5001/add_transaction  
+          
+          
+          
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
+
+
