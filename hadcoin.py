4668adad2501d5054d750513f49ddfdb78f2e0c5
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-27 12:28:45 +0545

    get chain
    get chain

--- hadcoin.py
+++ hadcoin.py
@@ -184,3 +184,10 @@ def mine_block():
                 'previous_hash': block['previous_hash'],
                 'transactions': block['transactions']}
     return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
