c551b82fdfa1fd0546a97dbd1e78db1099ed1e7a
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-27 12:30:22 +0545

    replace_chain
    replace_chain

--- hadcoin.py
+++ hadcoin.py
@@ -242,3 +242,15 @@ def connect_node():
     response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
                 'total_nodes': list(blockchain.nodes)}
     return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods = ['GET']) 
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
