d53ae165ee8119e527f415f0c55c8122db948846
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-27 12:12:31 +0545

    to be installed
    to be installed

---
+++ hadcoin.py
@@ -0,0 +1,14 @@
+# Module 2 - Create a Cryptocurrency
+
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
+# requests==2.18.4: pip install requests==2.18.4
+
+
+
+
+
+
+
+
